3590b760c1bc6df7ab3d3616431df005d1aab369
Nikita Mokhonko <nikitamokhonko@gmail.com>
2025-11-03 13:34:30 +0100

    Removed console.log

--- wifewebsite/src/main.tsx
+++ wifewebsite/src/main.tsx
@@ -32,7 +32,6 @@ declare module '@tanstack/react-router' {
 }
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-console.log('CLERK KEY:', PUBLISHABLE_KEY)
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
